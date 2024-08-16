bf87083b38ecb5b235d02936f46d70f340dd5190
phinsso <naso01247@gmail.com>
2024-08-16 14:26:03 +0900

    제네릭 실습 2

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CSAdv31
 {
     class Wanted<T>
@@ -14,6 +13,12 @@ namespace CSAdv31
             this.Value = value;
         }
     }
+    class WantedTest<T, U>
+        where T : IComparable
+        where U : IComparable, IDisposable
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -21,7 +26,6 @@ namespace CSAdv31
             Wanted<string> wantedString = new Wanted<string>("string");
             Wanted<int> wantedint = new Wanted<int>(52273);
             Wanted<double> wantedDouble = new Wanted<double>(52.273);
-
             Console.WriteLine(wantedString.Value);
             Console.WriteLine(wantedint.Value);
             Console.WriteLine(wantedDouble.Value);
