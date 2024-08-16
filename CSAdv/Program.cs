b840157ac729021aac66f3aebaa4364603a8999c
phinsso <naso01247@gmail.com>
2024-08-16 14:25:40 +0900

    제네릭 실습 1

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSAdv
+namespace CSAdv31
 {
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("string");
+            Wanted<int> wantedint = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedint.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
